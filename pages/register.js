322d51a28fc77a7a92cbd27113cdc7605e361a4b
marissao <50351548+marissao@users.noreply.github.com>
2022-01-16 12:47:33 -1000

    Removed default code, added pages

---
+++ pages/register.js
@@ -0,0 +1,10 @@
+const Register = () => {
+    return (
+     <div>
+        <h1>Register</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis accusamus qui, consectetur beatae saepe tempora quos minima pariatur eius consequatur tempore, fugiat provident similique sit ex. Veniam ipsum explicabo cumque?</p>
+     </div>
+    );
+}
+ 
+export default Register;
